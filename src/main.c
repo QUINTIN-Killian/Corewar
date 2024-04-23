4fb1bdc8c405c0ba84b644a50cf55ad100e85388
Killian QUINTIN <killian.quintin@epitech.eu>
2024-04-23 12:19:18 +0200

    [ADD] error handling pattern

--- src/main.c
+++ src/main.c
@@ -7,7 +7,29 @@
 
 #include "../include/corewar.h"
 
+void init_struct(corewar_t *corewar)
+{
+    corewar->champions = NULL;
+    corewar->nb_champions = 0;
+    corewar->nb_turns = -1;
+}
+
+int error_handling(int ac, char **av, corewar_t *corewar)
+{
+    init_struct(corewar);
+    if (ac < 3 || !is_enough_champions(ac, av))
+        return 1;
+    if (!extract_args(ac, av, corewar)) {
+        //fonction destroy linked list
+        return 1;
+    }
+}
+
 int main(int ac, char **av)
 {
-    return 84;
+    corewar_t corewar;
+
+    if (error_handling(ac, av, &corewar))
+        return 84;
+    return 0;
 }
