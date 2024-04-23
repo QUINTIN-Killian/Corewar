2fa4b4c0a2c3dac5b0531ae837022a14fd9e395c
Killian QUINTIN <killian.quintin@epitech.eu>
2024-04-23 14:30:59 +0200

    [FEAT] destroy struct

--- src/main.c
+++ src/main.c
@@ -14,15 +14,25 @@ void init_struct(corewar_t *corewar)
     corewar->nb_turns = -1;
 }
 
+void destroy_struct(corewar_t *corewar)
+{
+    delete_list(&corewar->champions);
+}
+
 int error_handling(int ac, char **av, corewar_t *corewar)
 {
     init_struct(corewar);
-    if (ac < 3 || !is_enough_champions(ac, av))
+    if (ac < 3 ) {
+        mini_fdprintf(2, "Not enough arguments.\n");
+        return 1;
+    }
+    if (!is_enough_champions(ac, av))
         return 1;
     if (!extract_args(ac, av, corewar)) {
         delete_list(&corewar->champions);
         return 1;
     }
+    corewar->champions = rev_champions(&corewar->champions);
     return 0;
 }
 
@@ -30,7 +40,11 @@ int main(int ac, char **av)
 {
     corewar_t corewar;
 
-    if (error_handling(ac, av, &corewar))
+    if (error_handling(ac, av, &corewar)) {
+        destroy_struct(&corewar);
         return 84;
+    }
+    display_champs_infos(&corewar.champions);
+    destroy_struct(&corewar);
     return 0;
 }
