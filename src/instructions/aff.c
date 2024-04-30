62cf79fc41f7eff6ca7834df6629f122a7798703
Killian QUINTIN <killian.quintin@epitech.eu>
2024-05-01 01:02:34 +0200

    [ADD] aff instruction

---
+++ src/instructions/aff.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2024
+** corewar-private
+** File description:
+** aff
+*/
+
+#include "../../include/corewar.h"
+
+void exec_aff(champion_t *champion)
+{
+    mini_printf("%c\n", champion->registers[convert_str_in_int(
+    champion->instructions->parameters[0])][0]);
+}
