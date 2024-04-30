68edb4cde42cf06845f6a3babe3e7970237f8717
Killian QUINTIN <killian.quintin@epitech.eu>
2024-04-30 20:04:05 +0200

    [ADD] live

---
+++ src/instructions/live.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2024
+** corewar-private
+** File description:
+** live
+*/
+
+#include "../../include/corewar.h"
+
+void exec_live(champion_t *champion)
+{
+    champion->cycle_live = -1;
+}
