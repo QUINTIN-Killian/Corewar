597d8d390911a75d9bdee934ee0bea54c1a37912
Killian QUINTIN <killian.quintin@epitech.eu>
2024-04-30 23:47:02 +0200

    [FIX] and, or & xor instructions take register value

--- src/instructions/or.c
+++ src/instructions/or.c
@@ -9,10 +9,20 @@
 
 void exec_or(champion_t *champion)
 {
+    int value1;
+    int value2;
     int ind_reg = convert_str_in_int(champion->instructions->parameters[2]);
 
+    if (my_strncmp(champion->instructions->coding_byte, "01", 2) == 0)
+        value1 = champion->registers[convert_str_in_int(
+        champion->instructions->parameters[0])];
+    else
+        value1 = convert_str_in_int(champion->instructions->parameters[0]);
+    if (my_strncmp(&(champion->instructions->coding_byte[2]), "01", 2) == 0)
+        value2 = champion->registers[convert_str_in_int(
+        champion->instructions->parameters[1])];
+    else
+        value2 = convert_str_in_int(champion->instructions->parameters[1]);
     free(champion->registers[ind_reg]);
-    champion->registers[ind_reg] = convert_int_to_str(convert_str_in_int(
-    champion->instructions->parameters[0]) |
-    convert_str_in_int(champion->instructions->parameters[1]));
+    champion->registers[ind_reg] = convert_int_to_str(value1 | value2);
 }
