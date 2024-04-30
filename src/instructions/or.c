622b1b8323e1ce9f58ad29c9a77a4c25e6eb5893
Killian QUINTIN <killian.quintin@epitech.eu>
2024-04-30 23:50:58 +0200

    [FIX] and, or & xor instructions

--- src/instructions/or.c
+++ src/instructions/or.c
@@ -9,18 +9,18 @@
 
 void exec_or(champion_t *champion)
 {
-    int value1;
-    int value2;
+    int value1 = 0;
+    int value2 = 0;
     int ind_reg = convert_str_in_int(champion->instructions->parameters[2]);
 
     if (my_strncmp(champion->instructions->coding_byte, "01", 2) == 0)
-        value1 = champion->registers[convert_str_in_int(
-        champion->instructions->parameters[0])];
+        value1 = convert_str_in_int(champion->registers[convert_str_in_int(
+        champion->instructions->parameters[0])]);
     else
         value1 = convert_str_in_int(champion->instructions->parameters[0]);
     if (my_strncmp(&(champion->instructions->coding_byte[2]), "01", 2) == 0)
-        value2 = champion->registers[convert_str_in_int(
-        champion->instructions->parameters[1])];
+        value2 = convert_str_in_int(champion->registers[convert_str_in_int(
+        champion->instructions->parameters[1])]);
     else
         value2 = convert_str_in_int(champion->instructions->parameters[1]);
     free(champion->registers[ind_reg]);
