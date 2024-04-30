33e5d27d8c639bdcbdab1ace01c6f02441072d6a
Killian QUINTIN <killian.quintin@epitech.eu>
2024-04-30 20:04:14 +0200

    [ADD] or

---
+++ src/instructions/or.c
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2024
+** corewar-private
+** File description:
+** or
+*/
+
+#include "../../include/corewar.h"
+
+void exec_or(champion_t *champion)
+{
+    int ind_reg = convert_str_in_int(champion->instructions->parameters[2]);
+
+    free(champion->registers[ind_reg]);
+    champion->registers[ind_reg] = convert_int_to_str(convert_str_in_int(
+    champion->instructions->parameters[0]) |
+    convert_str_in_int(champion->instructions->parameters[1]));
+}
