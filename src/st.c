f40b5eb79b0df0144139f87074f704c3a1be7cba
Zuhrdev <91844461+Elieleche@users.noreply.github.com>
2024-05-02 12:09:00 +0200

    [ADD] st with PC macro

--- src/st.c
+++ src/st.c
@@ -6,18 +6,18 @@
 */
 #include "../include/corewar.h"
 
-void exec_st(champion_t *champion)
+void exec_st(champion_t *champion, corewar_t *corewar)
 {
     int value1 = champion->registers[champion->instructions->parameters[0]];
     int value2 = 0;
     int adress = 0;
 
-    if (my_strncmp(champion->instructions->coding_byte[2], "01", 2) == 0) {
+    if (my_strncmp(&(champion->instructions->coding_byte[2]), "01", 2) == 0) {
         value2 = value1;
         champion->registers[champion->instructions->parameters[1]] = value2;
     } else {
         value2 = champion->instructions->parameters[1];
-        adress = champion->registers[0] + value2 % IDX_MOD;
-        champion->registers[adress] = value2;
+        adress = champion->PC + value2 % IDX_MOD;
+        set_memory_cell(corewar, value1, adress);
     }
 }
