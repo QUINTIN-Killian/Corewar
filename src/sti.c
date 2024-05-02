4d4c12752412ab91a91ac317233d94fd509181b4
Zuhrdev <91844461+Elieleche@users.noreply.github.com>
2024-05-02 12:15:14 +0200

    [ADD] sti instruction

--- src/sti.c
+++ src/sti.c
@@ -4,4 +4,25 @@
 ** File description:
 ** sti
 */
+#include "include/corewar.h"
 
+void exec_sti(champion_t *champion, corewar_t *corewar)
+{
+    int value1 = champion->registers[champion->instructions->parameters[0]];
+    int value2 = 0;
+    int value3 = 0;
+    int adress = 0;
+
+    if (my_strncmp(&(champion->instructions->coding_byte[2]), "01", 2) == 0) {
+        value2 = champion->registers[champion->instructions->parameters[1]];
+    } else {
+        value2 = champion->instructions->parameters[1];
+    }
+    if (my_strncmp(&(champion->instructions->coding_byte[4]), "01", 2) == 0) {
+        value2 = champion->registers[champion->instructions->parameters[2]];
+    } else {
+        value2 = champion->instructions->parameters[2];
+    }
+    adress = champion->PC + (value2 + value3) % IDX_MOD;
+    set_memory_cell(corewar, value1, adress);
+}
