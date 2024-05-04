ea72d9542d9e25c002ffb636dcf41fc8bff767ab
Zuhrdev <91844461+Elieleche@users.noreply.github.com>
2024-05-04 19:44:02 +0200

    [FIX] registrers check

--- include/corewar.h
+++ include/corewar.h
@@ -157,7 +157,7 @@ void exec_sti(champion_t *champion, corewar_t *corewar);
 //ld.c :
 int set_val(int i);
 void exec_ld(champion_t *champion, corewar_t *corewar);
-int check_ld(char *coding_byte, champion_t *champion);
+int check_ld(char *coding_byte, champion_t *champion, corewar_t *corewar);
 
 //lld.c :
 void exec_lld(champion_t *champion, corewar_t *corewar);
--- src/ld.c
+++ src/ld.c
@@ -51,22 +51,49 @@ static check_empty(int len, char *coding_byte, char *pair)
     }
 }
 
-int check_ld(char *coding_byte, champion_t *champion)
+static check_register(int coords, corewar_t *corewar)
+{
+    cell_t *cell = get_memory_cell(corewar, coords);
+
+    if (cell->value_int < 1 || cell->value_int > 16)
+        return 1;
+    return 0;
+}
+
+static int set_adresse(char *pair, champion_t *champion)
+{
+    if (my_strcmp(pair, "10") == 0) {
+        return champion->PC + 5;
+    } else if (my_strcmp(pair, "01") == 0) {
+        return champion->PC + 3;
+    } else if(my_strcmp(pair, "11") == 0) {
+        return champion->PC + 3;
+    }
+}
+
+int check_ld(char *coding_byte, champion_t *champion, corewar_t *corewar)
 {
     int len = my_strlen(coding_byte);
     char pair[3];
+    int adresse = 0;
+    cell_t *cell;
 
     pair[0] = coding_byte[0];
     pair[1] = coding_byte[1];
     pair[2] = '\0';
     if (my_strcmp(pair, "10") != 0 && my_strcmp(pair, "11") != 0 && my_strcmp(pair, "01") != 0)
         return 1;
+    if (adresse == champion->PC + 3 && my_strcmp(pair, "01") == 0 &&
+        check_register(champion->PC + 2, corewar) == 1) {
+        return 1;
+    }
+    adresse = set_adresse(pair, champion);
     pair[0] = coding_byte[2];
     pair[1] = coding_byte[3];
     if (my_strcmp(pair, "01") != 0)
         return 1;
-    if (champion->registers[champion->instructions->parameters[1]] < 1 ||
-        champion->registers[champion->instructions->parameters[1]] > 16)
+    cell = get_memory_cell(corewar, adresse);
+    if (cell->value_int < 1 || cell->value_int > 16)
         return 1;
     if (check_empty(len, coding_byte, pair) == 1)
         return 1;
