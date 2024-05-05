7f4b0f9f1b8566b7b65ffc94661515ffe3110a04
Zuhrdev <91844461+Elieleche@users.noreply.github.com>
2024-05-05 16:12:02 +0200

    [FIX] coding style aff.c

--- src/aff.c
+++ src/aff.c
@@ -14,6 +14,18 @@ void exec_aff(champion_t *champion)
     move_instruction_head(champion);
 }
 
+static check_end_cb(int len, char *pair, char *coding_byte)
+{
+    for (int i = 2; i < len; i += 2) {
+        pair[0] = coding_byte[i];
+        pair[1] = coding_byte[i + 1];
+        pair[2] = '\0';
+        if (my_strcmp(pair, "00") != 0)
+            return 1;
+    }
+    return 0;
+}
+
 int check_aff(char *coding_byte, champion_t *champion, corewar_t *corewar)
 {
     int len = my_strlen(coding_byte);
@@ -31,12 +43,7 @@ int check_aff(char *coding_byte, champion_t *champion, corewar_t *corewar)
     cell = get_memory_cell(corewar, adresse);
     if (cell->value_int < 1 || cell->value_int > 16)
         return 1;
-    for (int i = 2; i < len; i += 2) {
-        pair[0] = coding_byte[i];
-        pair[1] = coding_byte[i + 1];
-        pair[2] = '\0';
-        if (my_strcmp(pair, "00") != 0)
-            return 1;
-    }
+    if (check_end_cb(len, pair, coding_byte) == 1)
+        return 1;
     return 0;
 }
