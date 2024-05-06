74824cda8ddfe1f35f02d46acbf24ca5847143eb
Killian QUINTIN <killian.quintin@epitech.eu>
2024-05-06 21:29:02 +0200

    [FIX] fork & lfork instructions

--- src/fork.c
+++ src/fork.c
@@ -10,8 +10,8 @@
 void exec_fork(corewar_t *corewar, champion_t *champion)
 {
     int value = combine_bytes(2,
-    get_memory_cell(corewar, champion->PC + 1),
-    get_memory_cell(corewar, champion->PC + 2));
+    get_memory_cell(corewar, champion->PC + 1)->value_int,
+    get_memory_cell(corewar, champion->PC + 2)->value_int);
     champion_t *new_champion =
     duplicate_champion(&corewar->champions, champion);
 
