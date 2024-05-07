acd043fac7bde1b788fb20910cc13f9912ae4b9a
Elieleche <elie.chardin@epitech.eu>
2024-05-07 17:01:40 +0200

    [ADD] delete useless function and replace it by checkempy

--- src/aff.c
+++ src/aff.c
@@ -15,18 +15,6 @@ void exec_aff(corewar_t *corewar, champion_t *champion)
     champion->timeout = 2;
 }
 
-static int check_end_cb(int len, char *pair, char *coding_byte)
-{
-    for (int i = 2; i < len; i += 2) {
-        pair[0] = coding_byte[i];
-        pair[1] = coding_byte[i + 1];
-        pair[2] = '\0';
-        if (my_strcmp(pair, "00") != 0)
-            return 1;
-    }
-    return 0;
-}
-
 int check_aff(char *coding_byte, champion_t *champion, corewar_t *corewar)
 {
     int len = my_strlen(coding_byte);
@@ -42,7 +30,7 @@ int check_aff(char *coding_byte, champion_t *champion, corewar_t *corewar)
     cell = get_memory_cell(corewar, adresse);
     if (cell->value_int < 1 || cell->value_int > 16)
         return 1;
-    if (check_end_cb(len, pair, coding_byte) == 1)
+    if (check_empty(len, coding_byte, pair, 2) == 1)
         return 1;
     return 0;
 }
