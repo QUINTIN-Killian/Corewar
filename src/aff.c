9fc43ef293cbc715e75ec4213e30379c5663d09e
Killian QUINTIN <killian.quintin@epitech.eu>
2024-05-13 21:52:50 +0200

    [FIX] modulo 256 aff instruction

--- src/aff.c
+++ src/aff.c
@@ -10,7 +10,7 @@
 void exec_aff(corewar_t *corewar, champion_t *champion)
 {
     mini_printf("%c\n", champion->registers[get_memory_cell(corewar,
-    champion->PC + 1)->value_int]);
+    champion->PC + 1)->value_int] % 256);
     champion->PC = cycle_coords(champion->PC + 2);
     champion->timeout = 2;
 }
