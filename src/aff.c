b0c5b89bab93e59cffd16efff96252b0a68c7ab9
Killian QUINTIN <killian.quintin@epitech.eu>
2024-05-14 19:01:50 +0200

    [FIX] PC move after aff instruction

--- src/aff.c
+++ src/aff.c
@@ -11,7 +11,7 @@ void exec_aff(corewar_t *corewar, champion_t *champion)
 {
     mini_printf("%c\n", champion->registers[get_memory_cell(corewar,
     champion->PC + 2)->value_int] % 256);
-    champion->PC = cycle_coords(champion->PC + 2);
+    champion->PC = cycle_coords(champion->PC + 3);
     champion->timeout = 2;
 }
 
